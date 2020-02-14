faaf47485298a5086ac8d803c2083eda7bec9106
Stormy <russellsorin2@gmail.com>
2020-02-14 15:53:02 -0600

    new: discord interaction token works

--- discord/app.ts
+++ discord/app.ts
@@ -1,5 +1,26 @@
-import { magentaBright } from 'chalk';
+/* imports, TODO discord import optimization */
+import { magentaBright, yellowBright } from 'chalk';
+import { config } from 'dotenv';
+import * as discord from 'discord.js';
 
+/* fix variables */
+config();
+
+/* console prefix */
 const prefix = `${ magentaBright('discord') }>`;
 
-console.info(prefix, 'hello');
+/* discord things */
+const client = new discord.Client();
+client.login(process.env.discord_token).catch((error) => {
+  console.error(magentaBright(error));
+  console.error(magentaBright('  • is "discord_token" in your .env?'));
+  process.exit();
+});
+
+/**
+ * this is when the bot boots
+ * @event 'ready'
+*/
+client.once('ready', () => {
+  console.info(prefix, `${ yellowBright(client.user.tag) } signed in`);
+});
